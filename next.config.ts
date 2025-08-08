28265e2d242ec18f44ebe77d408d6657aa1e938b
KerelosNasser <157994690+KerelosNasser@users.noreply.github.com>
2025-08-09 02:04:29 +0300

    for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
+import {resolve} from "path";
+
 const nextConfig: NextConfig = {
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            "@": resolve(__dirname),
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
